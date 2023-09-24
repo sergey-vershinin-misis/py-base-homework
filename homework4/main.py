502d1b7ecb24f9c50ec8eec3207228c93c2f0030
Сергей Вершинин <Sergey@integro.local>
2023-09-25 00:52:38 +0500

    Add more changes

--- homework4/main.py
+++ homework4/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from tasks_1_to_3 import run_tasks_1_to_3
+from task_4 import run_task_4
+from tasks_5_to_6 import run_task_5_to_6
+from task_7 import run_task_7
+from task_8 import run_task_8
+from task_9 import run_task_9
+from task_10 import run_task_10
+from task_11 import run_task_11
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Каждое задание или совокупность связанных заданий находится в отдельном py-файле с именем 
+task_<номер/номера заданий>.py Задание запускается соответствующим run-методом. Имена таких 
+методов содержатся в методе main ниже. Для запуска конкретного задания раскомментируйте вызов 
+соответствующего методв
+"""
+def main():
+
+    run_tasks_1_to_3()
+    # run_task_4()
+    # run_task_5_to_6()
+    # run_task_7()
+    # run_task_8()
+    # run_task_9()
+    # run_task_10()
+    # run_task_11()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
